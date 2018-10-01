4fe65facfcacacc177a6a9f35bcdb24881a3222e
Bojan Lazic <lazicbojanns@gmail.com>
2018-10-01 20:44:20 +0200

    Include parallax

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-console.log('Shkrr!');
+$(function(){
+
+  initScroll();
+
+  function initScroll() {
+    // init controller
+    var controller = new ScrollMagic.Controller({globalSceneOptions: {triggerHook: "onEnter", duration: "200%"}});
+
+    // build scenes
+    new ScrollMagic.Scene({triggerElement: "#parallax1"})
+    .setTween("#parallax1 > div", {y: "80%", ease: Power2.easeOut})
+    .addTo(controller);
+  }
+  
+});
